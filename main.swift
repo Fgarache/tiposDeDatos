28d8cff726bfd283bd5dc7a57ade2c62319a6605
Francisco Garache <franciscogarach3@gmail.com>
2022-05-13 19:18:06 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,38 @@
+
+
+let orengeAreOrange = true
+let foodIsDelicious = false
+
+//bolean no declarada
+var isAged : Bool
+
+isAged = true
+
+if isAged { //entra si isaged == true
+  print ("Puedes entrar")
+}else {
+  print ("no puedes pasar")
+}
+
+var age = 31
+
+if age >= 18 {
+  print ("eres mayor de edad")
+}else{
+  print("eres menor de edad")
+}
+
+//TUPLAS
+
+let http404 = (404, "No encomtrado") //404=.0  No encontrado=.1
+let (statusCode, statusMesage) = http404
+
+print ("el codigo del estado: \(statusCode)")
+print("El msj es: \(statusMesage)")
+
+print("codigo: \(http404.0)")
+print("mensaje: \(http404.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+print("El código del estado es: \(http200Status.statusCode)")
+print("desciption : \(http200Status.description)")
